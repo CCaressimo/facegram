2a1c7d157f0cb32cc04ecb2bc522d8fd42a25d45
chris <c.caressimo@gmail.com>
2021-08-23 15:56:00 -0400

    first commit

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const es6Renderer = require("express-es6-template-engine");
+const bodyParser = require("body-parser");
+
---
+++ server/queries.js
@@ -0,0 +1,2 @@
+const Pool = require("pg").Pool;
+const bodyParser = require("body-parser");
